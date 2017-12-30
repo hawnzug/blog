451f9ee606094e1980f80efaf3f788f5eb197621
hawnzug <hawnzug@gmail.com>
2017-12-30 21:40:35 +0800

    modify theme

--- posts/2017-02-28-install-hsom.md
+++ posts/2017-02-28-install-hsom.md
@@ -2,46 +2,47 @@
 title: Install Haskell School of Music on Arch Linux
 ---
 
-```
+``` sh
 git clone git@github.com:Euterpea/HSoM.git && cd HSoM
 stack init --force --solver --install-ghc
 stack build
-```
-
-```
 sudo pacman -S portmidi fluidsynth soundfont-fluid
 ```
 
 test fluidsynth
-```
+``` sh
 fluidsynth -a alsa -m alsa_seq -l -i /usr/share/soundfonts/FluidR3_GM.sf2 bach_bourree.mid
 ```
 
 Edit `/etc/conf.d/fluidsynth`
-```
+``` sh
 SOUND_FONT=/usr/share/soundfonts/FluidR3_GM.sf2
 AUDIO_DRIVER=pulseaudio
 OTHER_OPTS='-m alsa_seq -r 48000'
 ```
 
-```
+Start daemon
+
+``` sh
 sudo cp /usr/lib/systemd/system/fluidsynth.service /usr/lib/systemd/user/
 systemctl start fluidsynth.service
 systemctl status fluidsynth.service
 ```
 
 Test daemon
-```
+``` sh
 aplaymidi -p128:0 bach_bourree.mid
 ```
 
-```
+Test Euterpea
+
+``` sh
 stack ghci
 import Euterpea
 playDev 2 $ c 4 en
 ```
 
 Convert midi to wav
-```
+``` sh
 fluidsynth -l -T wav /usr/share/soundfonts/FluidR3_GM.sf2 test.midi -F test.wav
 ```
--- site.hs
+++ site.hs
@@ -1,86 +1,63 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
 import           Hakyll
 import           Text.Pandoc.Options
 import           Text.Pandoc.Extensions
 import           Text.Pandoc.Shared (eastAsianLineBreakFilter)
 import           Data.Monoid ((<>))
 
+static = do
+  match "css/*" $ route idRoute >> compile compressCssCompiler
+  match "images/*" idCopy
+  match "js/*" idCopy
+  match "fonts/*" idCopy
+  where idCopy = route idRoute >> compile copyFileCompiler
+
 main :: IO ()
 main = hakyll $ do
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  static
+  let ldr ctx i = loadAndApplyTemplate "templates/default.html" ctx i >>= relativizeUrls
+  match "index.md" $ do
+    route   $ setExtension "html"
+    compile $ pandocChineseCompiler >>= ldr defaultContext
+  match "pgp.md" $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler >>= ldr defaultContext
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $ pandocChineseCompiler
+      >>= loadAndApplyTemplate "templates/post.html" postCtx
+      >>= saveSnapshot "content"
+      >>= ldr postCtx
+  create ["posts.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx = listField "posts" postCtx (return posts)
+                    <> constField "title" "Notes"
+                    <> defaultContext
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/posts.html" archiveCtx
+        >>= ldr archiveCtx
+  create ["feed.rss"] $ do
+    route idRoute
+    compile $ do
+      let feedCtx = postCtx <> bodyField "description"
+      posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+      renderAtom myFeedConfiguration feedCtx posts
+  match "templates/*" $ compile templateBodyCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-
-    match "js/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
-    match "fonts/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
-    match "index.md" $ do
-        route   $ setExtension "html"
-        compile $ pandocChineseCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
-    match "pgp.md" $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocChineseCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
-
-    create ["posts.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/posts.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-    create ["feed.rss"] $ do
-        route idRoute
-        compile $ do
-            let feedCtx = postCtx `mappend` bodyField "description"
-            posts <- fmap (take 10) . recentFirst =<<
-                loadAllSnapshots "posts/*" "content"
-            renderAtom myFeedConfiguration feedCtx posts
-
-    match "templates/*" $ compile templateBodyCompiler
-
-
---------------------------------------------------------------------------------
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postCtx = dateField "date" "%F" <> defaultContext
 
 readerOpts :: ReaderOptions
 readerOpts = def
+  { readerExtensions = pandocExtensions
+  }
 
 writerOpts :: WriterOptions
 writerOpts = def
   { writerHTMLMathMethod = KaTeX ""
+  , writerExtensions     = pandocExtensions
   }
 
 pandocChineseCompiler = pandocCompilerWithTransform readerOpts writerOpts eastAsianLineBreakFilter
